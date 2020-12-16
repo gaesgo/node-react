46814cac8210cdb9f1fe0b5c324c30a1c5b3d7a7
Gabriel Espling <gabrielespling@gmail.com>
2020-12-16 16:39:46 +0100

    Heroku test

--- api/Server.js
+++ api/Server.js
@@ -4,7 +4,7 @@ import helmet from 'helmet'
 import morgan from 'morgan'
 import bodyParser from 'body-parser'
 import Configuration from '../api/config/Configuration.js'
-import middlewares from './src/middlewares/Middlewares.js'
+// import middlewares from './src/middlewares/Middlewares.js'
 import UserRoutes from './src/routes/User.routes.js'
 
 
@@ -15,16 +15,18 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 app.use(helmet())
 app.use(morgan('common'))
+app.use(express.static("../portfolio/build"))
 
-app.get('/', (req,res) => {
-    res.send('Welcome!')
-})
+// app.get('/', (req,res) => {
+//    res.send('Welcome!')
+// })
 
 UserRoutes.routes(app)
-app.use(middlewares.notFound)
-app.use(middlewares.errorHandler)
+// app.use(middlewares.notFound)
+// app.use(middlewares.errorHandler)
 
 Configuration.connectToDatabase()
 Configuration.connectToPort(app)
+Configuration.connectToFrontend(app)
 
 export default app
--- api/config/Configuration.js
+++ api/config/Configuration.js
@@ -1,12 +1,13 @@
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
+import express from 'express'
 
 dotenv.config()
 
 const connectToDatabase = async () => {
 	try {
 		const DB_URL = process.env.DATABASE_URL
-		await mongoose.connect(DB_URL, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false })
+		await mongoose.connect(process.env.MONGODB_URI || DB_URL, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false })
 		console.log('SUCESSFULLY CONNECTED TO THE DATABASE!')
 	} catch (error) {
 		console.log('ERROR WHILE TRYING TO CONNECT TO THE DATABASE: ', error)
@@ -21,7 +22,15 @@ const connectToPort = (app) => {
 	})
 }
 
+const connectToFrontend = (app) => {
+	if(process.env.NODE_ENV === "production"){
+		app.use(express.static("../portfolio/build")) 
+	}	
+}
+
+
 export default {
 	connectToDatabase,
-	connectToPort
+	connectToPort,
+	connectToFrontend
 }
